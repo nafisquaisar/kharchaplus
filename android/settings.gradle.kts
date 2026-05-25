4cb1221565fa68aaac8705f07f7f8217e59dd011
Nafis <nafis@gmail.com>
2026-05-25 17:26:28 +0530

    fix gradle

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
